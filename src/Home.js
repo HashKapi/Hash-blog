0648e535174d5063a198c51b092217ea77d77f4a
KennyAckermanHub <hashim.hirji@gmail.com>
2021-10-31 17:01:29 -0400

    added fetching errors

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,33 @@ import BlogList from "./BlogList"
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
     const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() =>{
         fetch("http://localhost:8000/blogs")
-            .then(res => res.json())
+            .then(res => {
+                        
+                        if (!res.ok){
+                            throw error('Could not fetch data')
+                        }
+                        res.json()
+
+                    }
+            )
             .then(data => {
                     setBlogs(data)
                     setIsPending(false)
+                    setError(null)
                 }   
-            )
+            ).catch(e => {
+                setError(e.message)
+                setIsPending(false)
+            })
     },[])
 
     return ( 
         <div className="home">
+            {error && <p>{error}</p>}
             {isPending && <p>loading your data...</p>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
