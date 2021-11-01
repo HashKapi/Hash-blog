6b8766492de0a3562d023aec730111a448439f26
KennyAckermanHub <hashim.hirji@gmail.com>
2021-10-31 22:15:32 -0400

    Added a form and POST on new blog

--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,58 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('yoshi')
+    const [isPending, setIsPending] = useState(false)
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        const newBlog = {title, body, author}
+
+        setIsPending(true)
+        fetch("http://localhost:8000/blogs", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newBlog)
+        }).then(() => {
+            console.log('Blog added')
+            setIsPending(false)
+        })
+    }
+
     return (  
-        <p>About to create a blog</p>
+        <div className="create">
+            <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input 
+                    value={title}
+                    type="text" 
+                    required
+                    onChange={e => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea 
+                    value={body}
+                    required
+                    onChange={e => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select 
+                    value={author} 
+                    onChange={e => setAuthor(e.target.value)}
+                >
+                    <option value="yoshi">yoshi</option>
+                    <option value="mario">mario</option>
+                </select>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding blog...</button>}
+            </form>
+            <p>{title}</p>
+            <p>{body}</p>
+            <p>{author}</p>
+        </div>
     );
 }
  
